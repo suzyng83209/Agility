a9215a404f4896682dbd478129af87417042f026
Eric Dan <eric.dan8@gmail.com>
2017-06-24 19:01:27 -0400

    fix crashing due to bad imports

--- containers/AddTodoContainer.js
+++ containers/AddTodoContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { addTodo } from "../actions";
+import { connect } from "react-redux";
 import AddTodo from "../components/AddTodo";
+
+const mapStateToProps = state => ({ todos: state });
 
 const mapDispatchToProps = dispatch => ({
   addTodo: todo => dispatch(addTodo(todo))
 });
 
-export default connect({}, mapDispatchToProps)(AddTodo);
+export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
--- containers/TodoContainer.js
+++ containers/TodoContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { addTodo, toggleTodo, setVisibility } from "../actions";
-import { Todo as TodoComponent } from "../components/Todo";
+import Todo from "../components/Todo";
 
 const mapStateToProps = state => ({ todos: state });
 
@@ -11,4 +11,4 @@ const mapDispatchToProps = dispatch => ({
   setVisibility: filter => dispatch(setVisibility(filter))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
